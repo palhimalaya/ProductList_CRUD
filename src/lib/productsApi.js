c880ad22b3b93f275ebf4916697e44f211a233d0
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2023-06-02 18:46:24 +0545

    all api endpoint call

---
+++ src/lib/productsApi.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const baseURL = "https://product-fhqo.onrender.com/products";
+
+const getProducts = async () => {
+  try {
+    const response = await axios.get(baseURL);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getProduct = async (id) => {
+  try {
+    const response = await axios.get(`${baseURL}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const addProducts = async (product) => {
+  try {
+    const response = await axios.post(baseURL, product);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const editProduct = async (product) => {
+  try {
+    const response = await axios.put(`${baseURL}/${product.id}`, product);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const deleteProduct = async (id) => {
+  try {
+    const response = await axios.delete(`${baseURL}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export { getProducts, addProducts, getProduct, editProduct, deleteProduct };
