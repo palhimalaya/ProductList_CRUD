6a851391e74ef76d7f169233ce9c3bc1ff60be3b
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2023-06-03 00:29:19 +0545

    added bettter loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
-
 import "./App.css";
 import { useProductContext } from "./context/productContext";
 import ProductList from "./components/ProductList";
 import AddProductModal from "./components/AddProductModal";
 import SearchProducts from "./components/SearchProducts";
+import { ClimbingBoxLoader } from "react-spinners";
 
 function App() {
   const { isLoading, getProductsFromApi } = useProductContext();
@@ -17,7 +17,14 @@ function App() {
   return (
     <main>
       {isLoading ? (
-        <p>loading...</p>
+        <div className="loading">
+          <ClimbingBoxLoader color="#d67036" />
+          <p>loading...</p>
+          <p>
+            It may take more time at start because the backend hosted on render
+            free tier.
+          </p>
+        </div>
       ) : (
         <div className="container">
           <div className=" compContainer">
