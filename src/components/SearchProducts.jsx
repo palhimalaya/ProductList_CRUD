8c7993c375b2ffd4a412a873b87689300fb7105b
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2023-06-03 00:11:23 +0545

    made search more efficient

--- src/components/SearchProducts.jsx
+++ src/components/SearchProducts.jsx
@@ -4,23 +4,19 @@ import { useProductContext } from "../context/productContext";
 const SearchProducts = () => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const { products, setFilteredData, filteredData } = useProductContext();
+  const { products, setFilteredData } = useProductContext();
 
   const handleSearch = (event) => {
     const searchTerm = event.target.value.toLowerCase();
     setSearchQuery(searchTerm);
 
-    if (searchTerm === "") {
-      setFilteredData(products.products);
-    } else {
-      const filtered = filteredData.filter((item) => {
-        return (
-          item.product_name.toLowerCase().includes(searchTerm) ||
-          item.category_name.toLowerCase().includes(searchTerm)
-        );
-      });
-      setFilteredData(filtered);
-    }
+    const filtered = products.products.filter((item) => {
+      return (
+        item.product_name.toLowerCase().includes(searchTerm) ||
+        item.category_name.toLowerCase().includes(searchTerm)
+      );
+    });
+    setFilteredData(filtered);
   };
 
   return (
